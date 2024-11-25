8a1391ecee0979d3fd543f8c9e5169b201fd6b4f
gavin-d26 <51263285+gavin-d26@users.noreply.github.com>
2024-11-25 03:20:14 -0800

    config update

--- scripts/configs.py
+++ scripts/configs.py
@@ -1,7 +1,7 @@
 hp_configs = {
-    "run_name": "lm-1",
+    "run_name": "lm-2",
     "lr": 5e-4,
-    "batch_size": 256,
+    "batch_size": 129,
     "epochs": 50,
     "optimizer": "adamW",  # 'adamW'
     "dropout": 0.1,
@@ -16,6 +16,7 @@ hp_configs = {
     "min_lr": 1e-5,
     "num_warmpup_steps": 0.1,
     "gradient_clip_val": 1.0,
+    "grad_accumulation_steps": 2,
     # logging
     "wandb_flag": True,
     "project_name": "language_modeling",
@@ -25,5 +26,5 @@ hp_configs = {
     "gpu_idx": "4",
     # notes
     "notes": "based on learnings increasing epochs to 50, increasing batch size to 256, added gradient clipping -> 1\
-        , added warmup steps -> 0.1, kept dropout -> 0.1",
+        , added warmup steps -> 0.1, kept dropout -> 0.1, gradient accumulation steps -> 2",
 }
