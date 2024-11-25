c9ef82ca1d31f385dce96193683d1ea3c1183415
gavin-d26 <51263285+gavin-d26@users.noreply.github.com>
2024-11-25 01:36:31 -0800

    update configs

--- scripts/configs.py
+++ scripts/configs.py
@@ -2,9 +2,9 @@ hp_configs = {
     "run_name": "test-1",
     "lr": 1e-3,
     "batch_size": 128,
-    "epochs": 1,
+    "epochs": 10,
     "optimizer": "adamW",  # 'adamW'
-    "dropout": 0.4,
+    "dropout": 0.1,
     # dataloaders
     "tokenizer": "openai-community/gpt2",
     # transformer model
@@ -16,7 +16,7 @@ hp_configs = {
     "min_lr": 1e-5,
     "num_warmpup_steps": 50,
     # logging
-    "wandb_flag": False,
+    "wandb_flag": True,
     "project_name": "language_modeling",
     # system
     "num_proc": 2,
